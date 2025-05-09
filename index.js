83d3a39a3ccb43c2f25b9f5ff61f330f0a99b084
Arunkumar-Thiyagarajan <arunkumarhc2246@gmail.com>
2025-05-09 11:25:40 +0530

    back changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
-mongoose.connect("mongodb://localhost:27017/users")
+mongoose.connect("mongodb://127.0.0.1:27017/users")
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
